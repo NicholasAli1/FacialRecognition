cabe01d90c8710cc20c9882f37349a5dd036ff67
Nicholas Ali <nali12@mail.valenciacollege.edu>
2022-09-16 20:35:04 -0400

    Created Basic OpenCV Program

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv) {
+    Mat image;
+    image = imread("E:/Programming/C+/Computer Vision/lena.png", IMREAD_COLOR);
+    if (!image.data) {
+        printf("No image data \n");
+        return -1;
+    }
+    namedWindow("Display Image", WINDOW_AUTOSIZE);
+    imshow("Display Image", image);
+    waitKey(0);
+    return 0;
+
+};
+
